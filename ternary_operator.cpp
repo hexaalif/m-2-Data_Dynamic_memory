ecc76e72c7d34fb7b0116e63b7b7d7aa9e8776e8
hexaalif <hexaalif2020@gmail.com>
2023-05-26 10:05:58 +0600

    works

---
+++ ternary_operator.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    a % 2 == 0 ? cout << "even" : cout << "odd" << endl;
+    // cout << a << endl;
+    return 0;
+}
